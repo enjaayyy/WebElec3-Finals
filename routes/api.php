7cb6c0ee23789093b279f565d3e07b50888e5462
enjaayyy <enujeemontesaarubeosu@gmail.com>
2024-07-19 11:18:18 +0800

    final najud na commit

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+RoRoute::post('/login', [AuthController::class, 'login']);
+
+
+Route::middleware('auth:sanctum')->group(function (){
+    Route::get('movies', [MovieController::class, 'allmovies']);
+    Route::get('movies/{id}', [MovieController::class, 'movieindex']);
+    Route::get('directors/{id}', [MovieController::class, 'getDirector']);
+    Route::get('actors/{id}', [MovieController::class, 'getActor']);
+    Route::get('moviegenre', [MovieController::class, 'getMovieWithGenre']);
+    Route::get('movierate', [MovieController::class, 'getMovieWithRate']);
+});
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\MovieController;
-use App\Http\Controllers\AuthController;
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::post('/login', [AuthController::class, 'login']);
 
 
-Route::middleware('auth:sanctum')->group(function (){
-    Route::get('movies', [MovieController::class, 'allmovies']);
-    Route::get('movies/{id}', [MovieController::class, 'movieindex']);
-    Route::get('directors/{id}', [MovieController::class, 'getDirector']);
-    Route::get('actors/{id}', [MovieController::class, 'getActor']);
-    Route::get('moviegenre', [MovieController::class, 'getMovieWithGenre']);
-    Route::get('movierate', [MovieController::class, 'getMovieWithRate']);
+Route::get('/', function () {
+    return view('welcome');
 });
+
+
 
